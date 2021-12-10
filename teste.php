c79a35a199dcba00081778035f9fb8ce6d41a225
unknown <matheuzinho_pacheco@hotmail.com>
2021-12-10 03:00:04 -0300

    bis2bis

---
+++ teste.php
@@ -0,0 +1,100 @@
+<form>
+		Insira a pontuacao (Separado por ":"): <input type="text" name="joga">
+		<input type="submit" name="enviar">		
+</form>
+
+<?php 
+
+if (isset($_GET)) {
+	
+	foreach ($_GET as $key => $value) {
+
+		list($jogadora,$jogadorb)=explode(':', $value);
+		$jogador = $jogadora + $jogadorb;
+
+		$nova = ($jogador >= 40 ? 2 : 5);
+
+		if ($jogador < 5) {
+			echo "Saca Jogador A";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador < 10) {
+			echo "Saca jogador B";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador < 15) {
+			echo "Saca jogador A";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador < 20) {
+			echo "Saca jogador B";
+			echo "</br> $jogador";
+			break;
+		}
+
+		elseif ($jogador < 25) {
+			echo "Saca jogador A";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador < 30) {
+			echo "Saca jogador B";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador < 35) {
+			echo "Saca jogador A";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador < 40) {
+			echo "Saca jogador B";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador >= 40 && $jogador <=41) {
+			echo "Saca jogador A";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador >= 42 && $jogador <= 43) {
+			echo "Saca jogador B";
+			echo "</br> $jogador";
+			break;
+		}
+		elseif ($jogador >= 44 && $jogador <= 45) {
+			echo "Saca jogador A";
+			echo "</br> $jogador";
+			break;
+		}
+		if ($jogador >= 46) {
+		  $contaRodada = 0;
+		  $jogador1 = 'A';
+
+
+		for ($i=0; $i <= $jogador; $i++) { 
+			if (($contaRodada == $nova) && $jogador1 == 'B') {
+			          $jogador1 = 'A';
+			          $contaRodada = 0;
+			        }
+			else if (($contaRodada == $nova) && $jogador1 == 'A') {
+			          $jogador1 = 'B';
+			          $contaRodada = 0;
+			        }
+
+			        $contaRodada++;
+			      }
+			      echo "Saca jogador ".$jogador1;
+			      echo "</br> $jogador";
+			      break;
+
+		}
+
+	}
+}
+
+
+ ?>
